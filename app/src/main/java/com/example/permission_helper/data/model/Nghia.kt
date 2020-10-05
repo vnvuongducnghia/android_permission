a442b78c072d8a2106ad1a7d756aef5543508796
nghia.vuong <nghia.vuong@vn.defide.co.jp>
2020-10-05 09:29:55 +0700

    Create Nghia.kt
    Create Nghia

---
+++ app/src/main/java/com/example/permission_helper/data/model/Nghia.kt
@@ -0,0 +1,9 @@
+package com.example.permission_helper.data.model
+
+/**
+ * Created by nghia.vuong on 05,October,2020
+ */
+class Nghia {
+    private var Tuoi = 25
+    private var ChieuCao = "1m78"
+}
